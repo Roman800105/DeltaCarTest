56e2c13e2f419d6a33ad0e6e0d5dd0be88042a86
Roman800105 <111098373+Roman800105@users.noreply.github.com>
2022-08-12 17:37:40 +1000

    v.1

--- TestCSharp.cs
+++ TestCSharp.cs
@@ -25,16 +25,16 @@ namespace VehicleTest
         static void Main(string[] args)
         {
             // -------------------------------------------------------------------------------------
-            Vehicle vehicle1 = new PassengerCar(fuelConsumption:5, fuelCapacity:40, avgSpeed:80, passengerCapacity:4, passenger:0);
+            Vehicle vehicle1 = new PassengerCar(fuelConsumption:5, fuelCapacity:40, speed:80, passengerCapacity:4, passenger:0);
             Console.WriteLine(vehicle1);
 
-            Console.WriteLine($"Дальность по топливу({vehicle1.FuelСapacity}) {vehicle1.getEstimateRange(vehicle1.FuelСapacity)} км");
+            Console.WriteLine($"Дальность по топливу({vehicle1.FuelСapacity}) {vehicle1.GetEstimateRange(vehicle1.FuelСapacity)} км");
 
             vehicle1.Fuel = 35;
 
-            Console.WriteLine($"Дальность по топливу({vehicle1.Fuel}) {vehicle1.getEstimateRange(vehicle1.Fuel)} км");
+            Console.WriteLine($"Дальность по топливу({vehicle1.Fuel}) {vehicle1.GetEstimateRange(vehicle1.Fuel)} км");
 
-            Console.WriteLine($"Дальность по топливу(с учетом пассажиров:{((PassengerCar)vehicle1).Passenger}) {vehicle1.getActualRange()} км");
+            Console.WriteLine($"Дальность по топливу(с учетом пассажиров:{((PassengerCar)vehicle1).Passenger}) {vehicle1.GetActualRange()} км");
 
             ((PassengerCar)vehicle1).Passenger = 5;
             Console.WriteLine(vehicle1);
@@ -42,23 +42,23 @@ namespace VehicleTest
             ((PassengerCar)vehicle1).Passenger = 2;
             Console.WriteLine(vehicle1);
 
-            Console.WriteLine($"Дальность по топливу(с учетом пассажиров:{((PassengerCar)vehicle1).Passenger}) {vehicle1.getActualRange()} км");
-            Console.WriteLine($"Время в пути на 500км:{vehicle1.getTravelTime(500)}ч");
+            Console.WriteLine($"Дальность по топливу(с учетом пассажиров:{((PassengerCar)vehicle1).Passenger}) {vehicle1.GetActualRange()} км");
+            Console.WriteLine($"Время в пути на 500км:{vehicle1.GetTravelTime(500)}ч");
 
 
             Console.WriteLine();
 
             // -------------------------------------------------------------------------------------
-            Vehicle vehicle2 = new TruckCar(fuelConsumption:15, fuelCapacity:60, avgSpeed:60, cargoCapacity:1000, 0);
+            Vehicle vehicle2 = new TruckCar(fuelConsumption:15, fuelCapacity:60, speed:60, cargoCapacity:1000, 0);
             Console.WriteLine(vehicle2);
 
-            Console.WriteLine($"Дальность по топливу({vehicle2.FuelСapacity}) {vehicle1.getEstimateRange(vehicle2.FuelСapacity)} км");
+            Console.WriteLine($"Дальность по топливу({vehicle2.FuelСapacity}) {vehicle1.GetEstimateRange(vehicle2.FuelСapacity)} км");
 
             vehicle2.Fuel = 35;
 
-            Console.WriteLine($"Дальность по топливу({vehicle2.Fuel}) {vehicle1.getEstimateRange(vehicle2.Fuel)} км");
+            Console.WriteLine($"Дальность по топливу({vehicle2.Fuel}) {vehicle1.GetEstimateRange(vehicle2.Fuel)} км");
 
-            Console.WriteLine($"Дальность по топливу(с учетом груза:{((TruckCar)vehicle2).Cargo}) {vehicle2.getActualRange()} км");
+            Console.WriteLine($"Дальность по топливу(с учетом груза:{((TruckCar)vehicle2).Cargo}) {vehicle2.GetActualRange()} км");
 
             ((TruckCar)vehicle2).Cargo = 1500;
             Console.WriteLine(vehicle2);
@@ -66,16 +66,16 @@ namespace VehicleTest
             ((TruckCar)vehicle2).Cargo = 700;
             Console.WriteLine(vehicle2);
 
-            Console.WriteLine($"Дальность по топливу(с учетом груза:{((TruckCar)vehicle2).Cargo}) {vehicle2.getActualRange()} км");
-            Console.WriteLine($"Время в пути на 150км:{vehicle2.getTravelTime(150)}ч");
+            Console.WriteLine($"Дальность по топливу(с учетом груза:{((TruckCar)vehicle2).Cargo}) {vehicle2.GetActualRange()} км");
+            Console.WriteLine($"Время в пути на 150км:{vehicle2.GetTravelTime(150)}ч");
 
             Console.WriteLine();
 
             // -------------------------------------------------------------------------------------
-            Vehicle vehicle3 = new SportCar(fuelConsumption: 12, fuelCapacity: 50, avgSpeed: 160, speedMax: 300);
+            Vehicle vehicle3 = new SportCar(fuelConsumption: 12, fuelCapacity: 50, speed: 160);
             Console.WriteLine(vehicle3);
 
-            Console.WriteLine($"Дальность по топливу({vehicle3.FuelСapacity}) {vehicle3.getEstimateRange(vehicle3.FuelСapacity)} км");
+            Console.WriteLine($"Дальность по топливу({vehicle3.FuelСapacity}) {vehicle3.GetEstimateRange(vehicle3.FuelСapacity)} км");
 
             Console.ReadLine();
         }
@@ -86,55 +86,47 @@ namespace VehicleTest
     // Опишите класс автомобиль у которого есть базовые параметры в виде типа ТС, среднего расхода топлива, объем топливного бака, скорость. 
     abstract class Vehicle
     {
-        #region VechicleType Type // тип ТС 
-        public VechicleType Type { get => type; set => type = value; }
-        private VechicleType type; // тип ТС 
-        #endregion
+        public VechicleType Type { get; set; } // Тип ТС 
 
-        #region double FuelConsumption // среднего расхода топлива
-        public double FuelConsumption { get => fuelСonsumption; set => fuelСonsumption = value; }
-        private double fuelСonsumption;   // среднего расхода топлива   ? на 100км, уточнить
-        #endregion
+        //TODO .. на 100км, уточнить?
+        public double FuelConsumption { get; set; } // Средний расхода топлива
 
-        public double FuelСapacity { get => fuelСapacity; set => fuelСapacity = value; }
-        private double fuelСapacity;      // объем топливного бака
+        public double FuelСapacity { get; set; } // Объем топливного бака
 
-        public double AvgSpeed { get => avgSpeed; set => avgSpeed = value; }
-        double avgSpeed;          // скорость, средняя
+        // TODO ...средняя подразумевается, возможно нужно отдельное поле для текущей, уточнить?
+        public double Speed { get; set; } // Скорость
 
-        public double Speed { get => speed; set => speed = value; }
-        private double speed=0;             // скорость
-        
-        public double Fuel { get => fuel; set => fuel = value; }
-        private double fuel=0;              // объем топлива
+        public double Fuel { get; set; } // Топливо
 
-        public Vehicle(double fuelConsumption, double fuelCapacity, double avgSpeed)
+        public Vehicle(double fuelConsumption, double fuelCapacity, double speed) : this(fuelConsumption, fuelCapacity, speed, fuelCapacity)
+        {
+        }
+        public Vehicle(double fuelConsumption, double fuelCapacity, double speed, double fuel)
         {
             this.FuelConsumption = fuelConsumption;
             this.FuelСapacity = fuelCapacity;
-            this.AvgSpeed = avgSpeed;
-            this.Speed = 0;
-            this.Fuel = this.FuelСapacity;
+            this.Speed = speed;
+            this.Fuel = fuel;
         }
 
         //Опишите метод, с помощью которого можно вычислить сколько автомобиль может проехать на полном баке топлива или на остаточном количестве топлива в баке на данный момент.
-        public double getEstimateRange(double fuel)
+        public double GetEstimateRange(double fuel)
         {
-            return (fuel/fuelСonsumption)*100;
+            return (fuel / FuelConsumption) * 100;
         }
 
         //Метод для отображения текущей информации о состоянии запаса хода в зависимости от пассажиров и груза. 
-        public virtual double getActualRange()
+        public virtual double GetActualRange()
         {
-            return (fuel / fuelСonsumption) * 100;
+            return (Fuel / FuelConsumption) * 100;
         }
 
         //Метод, который на основе параметров количества топлива и заданного расстояния вычисляет за сколько автомобиль его преодолеет.
-        public double getTravelTime(double distance)
+        public double GetTravelTime(double distance)
         {
-            if (AvgSpeed > 0 && distance < getActualRange())
+            if (Speed > 0 && distance < GetActualRange())
             {
-                return distance / AvgSpeed;
+                return distance / Speed;
             } else
             {
                 return 0;
@@ -143,28 +135,29 @@ namespace VehicleTest
 
         public override string ToString()
         {
-            return $"Type:{Type}, FuelConsumption:{FuelConsumption}, FuelCapacity:{FuelСapacity}, AvgSpeed:{AvgSpeed}, Fuel:{Fuel}, Speed:{Speed}"; 
+            return $"Type:{Type}, FuelConsumption:{FuelConsumption}, FuelCapacity:{FuelСapacity}, Speed:{Speed}, Fuel:{Fuel}"; 
         }
     }
 
-    //У легкового автомобиля добавьте параметр количество перевозимых пассажиров. На основе данного параметра может изменяться запас хода. 
-    //Предусмотрите проверку на допустимое количество пассажиров.Каждый дополнительный пассажир уменьшает запас хода на дополнительные 6%.
+    // У легкового автомобиля добавьте параметр количество перевозимых пассажиров. На основе данного параметра может изменяться запас хода. 
+    // Предусмотрите проверку на допустимое количество пассажиров.Каждый дополнительный пассажир уменьшает запас хода на дополнительные 6%.
     class PassengerCar : Vehicle
     {
-        public int PassengerCapacity { get => passengerCapacity; set => passengerCapacity = value; }
-        private int passengerCapacity;                // пассажировместимость 
-        
+        public int PassengerCapacity { get; set; } // Пассажировместимость
+
+        // Предусмотрите проверку на допустимое количество пассажиров
+        // Проверка наивная... в реальном проекте их скорее всего будет 2(на уровне интерфейса и модели в методах типа Validate())
         public int Passenger { 
-            get => passenger; 
+            get => _passenger; 
             set { if (value >=0 && value <= PassengerCapacity) 
-                { passenger = value; } 
+                { _passenger = value; } 
                 else { Console.WriteLine($"(Passenger.set)Количество {value}, должно быть в диапазоне от нуля до {PassengerCapacity}"); } 
             } 
         }
-        private int passenger;                        // пассажиры
+        private int _passenger; // Количество пассажиров
 
-        public PassengerCar(double fuelConsumption, double fuelCapacity, double avgSpeed, int passengerCapacity, int passenger)
-            : base(fuelConsumption, fuelCapacity, avgSpeed)
+        public PassengerCar(double fuelConsumption, double fuelCapacity, double speed, int passengerCapacity, int passenger)
+            : base(fuelConsumption, fuelCapacity, speed)
         {
             this.Type = VechicleType.Passenger; 
             this.PassengerCapacity = passengerCapacity;
@@ -173,14 +166,14 @@ namespace VehicleTest
 
         const double passengerRangeReducing = 0.06; // Каждый дополнительный пассажир уменьшает запас хода на дополнительные 6%.
 
-        public override double getActualRange()
+        public override double GetActualRange()
         {
             if (Passenger > 0)
             {
-                return base.getActualRange() - base.getActualRange() * (passengerRangeReducing * passenger);
+                return base.GetActualRange() - base.GetActualRange() * (passengerRangeReducing * _passenger);
             } else
             {
-                return base.getActualRange();
+                return base.GetActualRange();
             }
         }
 
@@ -190,7 +183,9 @@ namespace VehicleTest
         }
     }
 
-    // Класс грузового автомобиля дополните параметром грузоподъемность.Также, как и у легкового автомобиля, грузоподъемность влияет на запас хода автомобиля. 
+    // Класс грузового автомобиля дополните параметром грузоподъемность.
+    // Также, как и у легкового автомобиля, грузоподъемность влияет на запас хода автомобиля. 
+    // Дополните класс проверкой может ли автомобиль принять полный груз на борт.
     class TruckCar : Vehicle
     {
         public double CargoCapacity { get => cargoCapacity; set => cargoCapacity = value; }
@@ -206,8 +201,8 @@ namespace VehicleTest
         }
         private double cargo;
 
-        public TruckCar(double fuelConsumption, double fuelCapacity, double avgSpeed, double cargoCapacity, double cargo) 
-            : base(fuelConsumption, fuelCapacity, avgSpeed)
+        public TruckCar(double fuelConsumption, double fuelCapacity, double speed, double cargoCapacity, double cargo) 
+            : base(fuelConsumption, fuelCapacity, speed)
         {
             this.Type = VechicleType.Truck;
             this.CargoCapacity = cargoCapacity;
@@ -216,15 +211,15 @@ namespace VehicleTest
 
         const double cargoRangeReducing = 0.04; // Каждые дополнительные 200кг веса уменьшают запас хода на 4%.
 
-        public override double getActualRange()
+        public override double GetActualRange()
         {
             if (Cargo > 0)
             {
-                return base.getActualRange() - base.getActualRange() * (cargoRangeReducing * (Cargo/200));
+                return base.GetActualRange() - base.GetActualRange() * (cargoRangeReducing * (Cargo/200));
             }
             else
             {
-                return base.getActualRange();
+                return base.GetActualRange();
             }
         }
 
@@ -238,27 +233,10 @@ namespace VehicleTest
     // спортивный автомобиль
     class SportCar : Vehicle
     {
-        //TODO отсебятина... в принципе задание типа хватит...для него
-        public double SpeedMax { get => speedMax; set => speedMax = value; }
-        private double speedMax;
-
-        public SportCar(double fuelConsumption, double fuelCapacity, double avgSpeed, double speedMax)
-            : base(fuelConsumption, fuelCapacity, avgSpeed)
+        public SportCar(double fuelConsumption, double fuelCapacity, double speed)
+            : base(fuelConsumption, fuelCapacity, speed)
         {
             this.Type = VechicleType.Sport;
-            this.SpeedMax = speedMax;
-        }
-
-        /*
-        public override double getActualRange()
-        {
-            return base.getActualRange();
-        }
-        */
-
-        public override string ToString()
-        {
-            return base.ToString() + $", SpeedMax:{SpeedMax}";
         }
     }
 }
